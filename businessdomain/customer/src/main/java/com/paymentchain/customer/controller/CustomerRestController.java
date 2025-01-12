90b25dab0667a0e0269c89bceb815e10990ce7f5
NachoPC <ignaciodm93@gmail.com>
2025-01-12 15:42:21 -0300

    no message

--- businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -1,6 +1,8 @@
 package com.paymentchain.customer.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.entities.CustomerProduct;
 import com.paymentchain.customer.respository.CustomerRepository;
 
 import io.netty.channel.ChannelOption;
@@ -12,15 +14,21 @@ import reactor.netty.http.client.HttpClient;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -74,12 +82,43 @@ public class CustomerRestController {
 		customerRepository.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	@GetMapping("/full")
+	public Customer getByCode(@RequestPart String code) {
+		Customer customer = customerRepository.findByCode(code);
+		List<CustomerProduct> products = customer.getProducts();
+		products.forEach(p -> {
+			p.setProductName(getProductName(p.getId()));
+		});
+		return customer;
+	}
 
-	HttpClient client = HttpClient.create().option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-			.option(ChannelOption.SO_KEEPALIVE, true).option(EpollChannelOption.TCP_KEEPIDLE, 300)
-			.option(EpollChannelOption.TCP_KEEPINTVL, 60).responseTimeout(Duration.ofSeconds(1))
-			.doOnConnected(connection -> {
-				connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
-				connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
-			});
+	private HttpClient getHttpClientTimeoutConfig() {
+		return HttpClient.create().option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+				.option(ChannelOption.SO_KEEPALIVE, true).option(EpollChannelOption.TCP_KEEPIDLE, 300)
+				.option(EpollChannelOption.TCP_KEEPINTVL, 60).responseTimeout(Duration.ofSeconds(1))
+				.doOnConnected(connection -> {
+					connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+					connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+				});
+	}
+	
+	private String getProductName(Long id) {
+		WebClient client = buildWebClient();	
+		JsonNode response = getProductFromProductsMicroservice(id, client);	
+		return response.get("name").asText();
+	}
+
+	private WebClient buildWebClient() {
+		return webClientBuilder.clientConnector(new ReactorClientHttpConnector(getHttpClientTimeoutConfig()))
+				.baseUrl("http://localhost:8083/product")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+				.build();
+	}
+
+	private JsonNode getProductFromProductsMicroservice(Long id, WebClient client) {
+		return client.method(HttpMethod.GET).uri("/" + id)
+				.retrieve().bodyToMono(JsonNode.class).block();
+	}
 }
--- businessdomain/customer/src/main/java/com/paymentchain/customer/respository/CustomerRepository.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/respository/CustomerRepository.java
@@ -2,7 +2,13 @@ package com.paymentchain.customer.respository;
 
 import com.paymentchain.customer.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    
+
+	@Query("select c from customer c where c.code = ?1")
+	public Customer findByCode(String code);
+	
+	@Query("select c from customer c where c.iban = ?1")
+	public Customer findByAccount(String iban);
 }
